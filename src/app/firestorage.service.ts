48b4f7efb78b3fc15e1f22206fd8b32637214193
VeronicaFS20 <veronica.frutos@gmail.com>
2020-06-15 09:45:11 +0200

    subida de img a firestore

--- src/app/firestorage.service.ts
+++ src/app/firestorage.service.ts
@@ -15,12 +15,24 @@ export class FirestorageService {
   downloadURL = of('');
 
 
-  constructor(public firestorage: AngularFireStorage,
+  constructor(
+    public firestorage: AngularFireStorage,
     public auth: AuthService) { }
 
   upload(event) {
     console.log('event: ', event);
     
+    let ext = '.jpg';
+    if (event.target.files[0].type === 'image/png') {
+      ext = '.png';
+    }
+
+    this.firestorage.upload('images/' + this.auth.authUser.uid + ext , event.target.files[0])
+    .then(result => {
+      console.log('result', result);
+    }).catch(error =>{
+      console.log('error', error);
+    });
 
   }
 
